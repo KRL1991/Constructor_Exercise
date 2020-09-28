ed9687395dcb5e3692d5ae472ac7e54e7a3fd9fb
KRL1991 <kimx4706@edu.easj.dk>
2020-09-28 10:42:17 +0200

    First upload

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+// Studerende i studiegruppen
+        Studerende st1 = new Studerende();
+        st1.navn = "Mikkel";
+        st1.alder = 24;
+        st1.by = "Haslev";
+
+
+        Studerende st2 = new Studerende();
+        st2.navn = "Anders";
+        st2.alder = 28;
+        st2.by = "Køge";
+
+
+        Studerende st3 = new Studerende();
+        st3.navn = "Mathias";
+        st3.alder = 20;
+        st3.by = "Næstved";
+
+
+        Studerende st4 = new Studerende();
+        st4.navn = "Mike";
+        st4.alder = 26;
+        st4.by = "København";
+
+
+        Studerende st5 = new Studerende();
+        st5.navn = "Kim";
+        st5.alder = 28;
+        st5.by = "Næstved";
+
+
+        // Scanner som finder alderen på de studerende i studiegruppe
+
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Hvis alder i studiegruppen, vil du slå op? ");
+            String nameToFind = scanner.next();
+
+            switch (nameToFind) {
+                case "Mikkel":
+                    System.out.println(st1.alder);
+
+                    break;
+
+                case "Anders":
+                    System.out.println(st2.alder);
+
+                    break;
+
+                case "Mathias":
+                    System.out.println(st3.alder);
+
+                    break;
+
+                case "Mike":
+                    System.out.println(st4.alder);
+
+                    break;
+
+                case "Kim":
+                    System.out.println(st5.alder);
+
+                    break;
+
+                default:
+                    System.out.println(nameToFind + " Personens navn findes ikke i databasen.");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ src/Studerende.java
@@ -0,0 +1,18 @@
+public class Studerende {
+
+    String navn;
+    int alder;
+    String by;
+    String studieretning;
+    String campus;
+
+
+Studerende (){
+
+
+    studieretning = "Datamatikker";
+    campus = "Næstved";
+
+    System.out.println(" Ny Studerende indlæst");
+}
+}
